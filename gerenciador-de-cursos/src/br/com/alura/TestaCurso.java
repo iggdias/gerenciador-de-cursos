b1a075d440cf4b8c833d51afef024b850cc6f592
iggdias <dias.igor.ti@gmail.com>
2018-08-12 09:38:07 -0300

    add class TestaCurso

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,19 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaCurso {
+	public static void main(String[] args) {
+		Curso cursoDaVida = new Curso("Como ser pai", "ninguém");
+
+		Aula aula = new Aula("como nao dormir", 50);
+		cursoDaVida.adiciona(aula);
+		
+		cursoDaVida.adiciona(new Aula("como ganhar dinheiro", 25));
+		cursoDaVida.adiciona(new Aula("como perder dinheiro", 1));
+
+		List<Aula> aulas = cursoDaVida.getAulas();
+		System.out.println(aulas.get(0));
+	}
+}
