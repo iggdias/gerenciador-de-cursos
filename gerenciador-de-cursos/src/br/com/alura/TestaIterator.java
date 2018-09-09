21b793c608b8c625807f36ebceed7daaad8e3b50
iggdias <dias.igor.ti@gmail.com>
2018-09-09 10:05:54 -0300

    add test class TestaIterator

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaIterator.java
@@ -0,0 +1,28 @@
+package br.com.alura;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestaIterator {
+
+	public static void main(String[] args) {
+
+		List<String> letras = new LinkedList<>();
+		letras.add("A");
+		letras.add("B");
+		letras.add("C");
+
+//		letras.forEach(letra -> {
+//		    System.out.println(letra);
+//		});
+		
+		Iterator<String> it = letras.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+	}
+
+}
